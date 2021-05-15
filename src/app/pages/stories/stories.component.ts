d4a11659f7f4b2a447559f63a06646e804fb18db
Javaluca <g.pellegrini89@gmail.com>
2021-05-15 18:35:06 +0200

    Some improvement

--- src/app/pages/stories/stories.component.ts
+++ src/app/pages/stories/stories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { finalize } from 'rxjs/operators';
 import { Story } from 'src/app/models/story';
 import { HackerNewsService } from 'src/app/services/hacker-news.service';
 
@@ -11,10 +12,24 @@ export class StoriesComponent implements OnInit {
 
   private _stories: Story[];
 
+  private _loading: boolean = true;
+
   constructor(private readonly hnService: HackerNewsService) { }
 
   ngOnInit(): void {
-    this.hnService.topStories().subscribe(stories => this._stories = stories);
+    this.loadNews();
+  }
+
+  get loading(): boolean {
+    return this._loading || false;
+  }
+
+  private loadNews() {
+    this._loading = true;
+    this.hnService.topStories().subscribe(
+      stories => this._stories = stories,
+      error => console.log(error),
+      () => this._loading = false);
   }
 
   get stories(): Story[] {
